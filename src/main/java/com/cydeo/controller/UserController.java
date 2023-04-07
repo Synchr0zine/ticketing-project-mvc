0678c8b9d64e7bf2c7009c4b89fe876a3f2dd8b9
Baturay Gok <baturay.gok@gmail.com>
2023-04-07 07:48:07 -0500

    Created RoleDtoConverter to fix the exception, fixed a bug in the create.html

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,6 +6,8 @@ import com.cydeo.service.impl.UserServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,10 +24,22 @@ public class UserController {
 
 
     @GetMapping("/create")
-    public String createUser(Model model){
+    public String createUser(Model model) {
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
         model.addAttribute("users", userService.findAll());
         return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user") UserDTO user, Model model) {
+
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        userService.save(user);
+        model.addAttribute("users", userService.findAll());
+
+        return "/user/create";
+
     }
 }
