ad4e2a6df10240241eaacebb5fba4a7a1e5b1888
Baturay Gok <baturay.gok@gmail.com>
2023-05-08 15:22:17 -0500

    Created ProjectController and its implementation

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,6 +1,10 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.UserService;
+import com.cydeo.service.impl.UserServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectController {
 
     @GetMapping("/create")
-    public String createProject(){
+    public String createProject(Model model){
 
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("managers",)
         return "/project/create";
     }
 }
---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,8 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import org.yaml.snakeyaml.events.Event;
+
+public interface ProjectService extends CrudService<ProjectDTO,String> {
+
+}
