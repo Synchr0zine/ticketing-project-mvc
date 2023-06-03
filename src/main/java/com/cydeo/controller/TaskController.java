23ee96d0f70d5a0d1ad2282bb4b5a42091b6c2f5
Baturay Gok <baturay.gok@gmail.com>
2023-06-03 02:17:02 -0500

    Update function on create task page

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,15 +1,13 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/task")
@@ -51,6 +49,37 @@ public class TaskController {
         taskService.deleteById(id);
 
         return "redirect:/task/create";
+    }
+
+    @GetMapping("/update/{taskId}")
+    public String editTask(@PathVariable("taskId") Long taskId, Model model ){
+
+        model.addAttribute("task", taskService.findById(taskId));
+        model.addAttribute("employees",userService.findEmployees());
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("tasks",taskService.findAll());
+
+        return "/task/update";
+
+    }
+
+   /* @PostMapping("/update/{taskId}")
+    public String updateTask(@PathVariable("taskId") Long taskId,TaskDTO task){
+
+        task.setId(taskId);
+        taskService.update(task);
+
+        return "redirect:/task/create";
+
+    }*/
+
+    @PostMapping("/update/{id}")   //spring automatically parses it if the name is same
+    public String updateTask(TaskDTO task){
+
+        taskService.update(task);
+
+        return "redirect:/task/create";
+
     }
 
 }
